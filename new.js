cc221e0f1bba4c4f115bf2f5cde8e5903cc594f0
steve zhang <871506263@qq.com>
2022-02-26 00:31:34 +0800

    提交代码于: 2022-02-26 00:31:34

--- new.js
+++ new.js
@@ -5,13 +5,17 @@
  * 3. 执行构造函数Fn的代码，往新创建的对象obj上添加成员属性和方法
  * 4. 返回这个新的对象obj
  */
-
- const _new = function (func, ...args) {
+function _new(func, ...args) {
+  console.log('func:', func);
+  console.log('args:', args);
   if (typeof func !== 'function') {
-    throw 'func must be a function'
+    throw new Error('func must be a function');
   }
+
   // 这里有点求快了，应该手动模拟一下
-  let obj = Object.create(func.prototype)
+  /**
+  let obj = Object.create(func.prototype);
+  */
   // 实际模拟如下
   /**
   let Ctor = function () {}
@@ -19,24 +23,36 @@
   Ctor.prototype.constructor = func
   let obj = new Ctor()
  */
-  let result = func.apply(obj, args)
 
-  if (typeof result === 'object' && result !== null || typeof result === 'function') {
-    return result
+  let Ctor = function () {};
+  Ctor.prototype = func.prototype; // Ctor.prototype === Person.prototype
+  Ctor.prototype.constructor = func; // Ctor.prototype.constructor === Person
+  let obj = new Ctor();
+  console.log('obj:', obj);
+
+  let result = func.apply(obj, args);
+  console.log('result:', result);
+
+  if ((typeof result === 'object' && result !== null) || typeof result === 'function') {
+    console.log('---1---');
+    return result;
   } else {
-    return obj
+    console.log('---2---');
+    return obj;
   }
 }
 
-let Person = function (name, sex) {
-  this.name = name
-  this.sex = sex
+function Person(name, sex) {
+  this.name = name;
+  this.sex = sex;
 }
 
 Person.prototype.showInfo = function () {
-  console.log(this.name, this.sex)
-}
+  console.log('showInfo执行了～');
+  console.log('输出:', this.name, this.sex);
+};
 
-let p1 = _new(Person, '前端胖头鱼', 'sex')
+let p1 = _new(Person, '前端胖头鱼', 'sex');
 
-console.log(p1)
+console.log('p1:', p1);
+p1.showInfo();
